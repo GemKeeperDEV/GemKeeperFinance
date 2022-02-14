fd87cf8586b8c3cddc2e65d0996bc32c6075c2e2
BlingDev <98753950+BlingDev@users.noreply.github.com>
2022-02-14 14:40:08 +0300

    Update Bling.sol

--- contracts for review/Bling.sol
+++ contracts for review/Bling.sol
@@ -7,13 +7,13 @@ import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract Bling is ERC20, Ownable, ERC20Permit {
     uint256 public totalCap;
-    uint256 public INVESTORS_SHARE = 10;
-    uint256 public TREASURY_SHARE = 10; // Treasury for future projects
-    uint256 public ECOSYSTEM_SHARE = 5; // Ecosystem Collabration
+    uint256 public INVESTORS_SHARE = 100;
+    uint256 public TREASURY_SHARE = 100; // Treasury for future projects
+    uint256 public ECOSYSTEM_SHARE = 75; // Ecosystem Collabration
     constructor(string memory name, string memory symbol, uint256 _totalCap) ERC20(name, symbol) ERC20Permit(name) {
-        _mint(msg.sender, _totalCap * INVESTORS_SHARE / 100);
-        _mint(msg.sender, _totalCap * TREASURY_SHARE / 100);
-        _mint(msg.sender, _totalCap * ECOSYSTEM_SHARE / 100);
+        _mint(msg.sender, _totalCap * INVESTORS_SHARE / 1000);
+        _mint(msg.sender, _totalCap * TREASURY_SHARE / 1000);
+        _mint(msg.sender, _totalCap * ECOSYSTEM_SHARE / 1000);
         totalCap = _totalCap;
     }
 
